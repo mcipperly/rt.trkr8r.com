2ecc9bc85f7008132eb56c21330d1fabcb69f12e
Matthew Cipperly <mc@dc.wtfpgh.com>
2015-02-21 16:05:00 -0500

    1424552700 commit

--- signature.php
+++ signature.php
@@ -52,23 +52,18 @@
         saveButton = wrapper.querySelector("[data-action=save]"),
         canvas = wrapper.querySelector("canvas"),
         signaturePad;
-
     function resizeCanvas() {
         var ratio = window.devicePixelRatio || 1;
         canvas.width = canvas.offsetWidth * ratio;
         canvas.height = canvas.offsetHeight * ratio;
         canvas.getContext("2d").scale(ratio, ratio);
     }
-
     window.onresize = resizeCanvas;
     resizeCanvas();
-
     signaturePad = new SignaturePad(canvas);
-
     clearButton.addEventListener("click", function(event) {
         signaturePad.clear();
     });
-
     saveButton.addEventListener("click", function(event) {
         if (signaturePad.isEmpty()) {
             alert("Signature is required!");
