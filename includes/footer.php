3d0de409a8d28c866b947b4c9d1ac2e3c0622df0
Matthew Cipperly <mc@dc.wtfpgh.com>
2015-02-21 13:00:00 -0500

    1424541600 commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,18 +1,7 @@
         <footer>
-            <div class="row visible-desktop visible-tablet">
-                <div class="six cols">
-                    <p><a href="vjohnson@rtpittsburgh.org">Contact Us With Any Questions</a> &#8226; <a href="#">Staff Login</a>
-                    </p>
-                </div>
-                <div class="six cols">
-                    <p class="right hidden-phone">&copy;Rebuilding Together Pittsburgh 2015. 
-                    </p>
-                </div>
-            </div>
-
-            <div class="row visible-phone">
+          <div class="row">
                 <div class="twelve cols center">
-                    <p>&copy;Rebuilding Together Pittsburgh 2015. &#8226; <a href="#">Staff Login</a> &#8226; <a href="vjohnson@rtpittsburgh.org">Contact Us With Any Questions</a>
+                    <p>&copy;Rebuilding Together Pittsburgh 2015.&nbsp;&nbsp;/&nbsp;&nbsp; <a href="#">Staff Login</a>&nbsp;&nbsp;/&nbsp;&nbsp;<a href="vjohnson@rtpittsburgh.org">Contact Us With Any Questions</a>
                     </p>
                 </div>
         </footer>
---
+++ register.php
@@ -0,0 +1,13 @@
+<?php include ( 'includes/header.php'); ?>
+
+<div class="center">
+    <img src="assets/imgs/rt-logo.png" class="logo">
+</div>
+
+<h1 class="center">Register</h1>
+
+
+</div>
+
+
+<?php include ( 'includes/footer.php'); ?>
