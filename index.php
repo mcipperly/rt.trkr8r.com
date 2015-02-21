e5a12ffbb22e74e65459b3f49fb6a1ec76599ee2
Matthew Cipperly <mc@dc.wtfpgh.com>
2015-02-21 11:44:43 -0500

    1424537083 commit

--- index.php
+++ index.php
@@ -3,25 +3,23 @@
 <div class="center">
     <img src="assets/imgs/rt-logo.png">
 </div>
-<h1>Welcome</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique blandit nisl. Vivamus pretium pretium lectus, non imperdiet arcu volutpat id. Maecenas tempor scelerisque enim quis placerat. Donec vel erat pulvinar, volutpat felis vel, consequat eros. Morbi non velit euismod, pellentesque leo sed, ultricies enim.</p>
 
-<h2>Choose Which Fits You Best</h2>
+<h1 class="center">Welcome! Choose Which Fits You Best</h1>
 <div class="row center">
     <div class="four cols">
-        <a href="register.php">
+        <a href="#">
             <button>I am Registering On-Site</button>
         </a>
     </div>
 
     <div class="four cols">
-        <a href="register.php/?pre=1">
+        <a href="#">
             <button>I am Pre-Registering</button>
         </a>
     </div>
 
     <div class="four cols">
-        <a href="verify.php/?pre=1">
+        <a href="#">
             <button>I Already Pre-Registered</button>
         </a>
     </div>
