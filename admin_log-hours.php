268e2ccf54379003ddb3b2697c0c14a94b0d5ca4
Matthew Cipperly <mc@dc.wtfpgh.com>
2015-02-22 00:35:00 -0500

    1424583300 commit

--- admin_log-hours.php
+++ admin_log-hours.php
@@ -1,14 +1,26 @@
 <?php include ('includes/header.php'); ?>
 
 <div class="row interior-header">
-    <div class="eight cols">
-        <h1>Admin <span>Log Volunteer Hours</span></h1>
-  </div>
-    
-  <div class="four cols sml-logo">
-    <img src="assets/imgs/rt-logo.png">
-  </div>
-  
+
+    <div class="visible-phone">
+        <div class="four cols sml-logo">
+            <img src="assets/imgs/rt-logo.png">
+        </div>
+
+        <div class="eight cols">
+            <h1>Admin <span>Log Volunteer Hours</span></h1>
+        </div>
+    </div>
+
+    <div class="hidden-phone">
+        <div class="eight cols">
+            <h1 class="left">Admin <span>Log Volunteer Hours</span></h1>
+        </div>
+        
+        <div class="four cols">
+            <img src="assets/imgs/rt-logo_small.png" class="right">
+        </div>
+    </div>
 </div>
 
 
