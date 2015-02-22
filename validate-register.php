6244d46a90dab4837b0f875b35c6c83aa7478936
Matthew Cipperly <mc@dc.wtfpgh.com>
2015-02-22 08:55:00 -0500

    1424613300 commit

--- validate-register.php
+++ validate-register.php
@@ -1,4 +1,13 @@
 <?php
 include('db/db.php');
 
+$elements = get_form_elements();
+$req_elements = array();
+
+foreach($elements as $element) {
+  if($element['required'] == 1) {
+    $req_elements[$element['name']] = $element['label']; 
+  }
+}
+
 ?>
