03ebcb0717bdf93a59e51b9f63ac7afe828b5151
Matthew Cipperly <mc@dc.wtfpgh.com>
2015-02-22 06:05:00 -0500

    1424603100 commit

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 require_once('db/db.php'); 
- 
+session_start(); 
 $volunteer_id = validate_volunteer_email($_REQUEST['email']);
-if(isset($_REQUEST['email']) && $volunteer_id && !$_REQUEST['edit']) {
+if(isset($_REQUEST['email']) && $volunteer_id && !$_REQUEST['edit'] && isset($_SESSION['mode'])) {
   Header("HTTP/1.1 302 Moved Temporarily");
   Header("Location: signature.php?vid=" . $volunteer_id);
 } 
