51e1b255df6d82aa8e79a7bd390989cff92d599d
rob <topia17@gmail.com>
2015-04-03 21:16:06 -0400

    new functions to add, update, remove, and fetch forms

--- register.php
+++ register.php
@@ -19,13 +19,13 @@ function get_response($response) {
 
 $form_id = 1; //Hard-coded for now, until ability to choose forms is available
 
-$elements = get_form_elements($form_id);
+$form = get_form($form_id);
 if($volunteer_id)
 	$responses = get_form_responses($volunteer_id);
 
 $col_count = 0;
 $asterisk_count = 0;
-foreach($elements as $key => $element) {
+foreach($form['elements'] as $key => $element) {
 	if($key == 0) {
 		$html = <<<EOS
 <div class="row interior-header">
@@ -246,7 +246,7 @@ EOS;
 		print($html);
 	}
 
-	if($key + 1 == sizeof($elements)) {
+	if($key + 1 == sizeof($form['elements'])) {
 		$html = <<<EOS
     <input type="submit" value="Submit">
 
@@ -266,7 +266,7 @@ EOS;
 
 
 $asterisk_count = 0;
-foreach($elements as $element) {
+foreach($form['elements'] as $element) {
 	if($element['description']) {
 		$asterisk_count++;
 		$asterisk_string = "";
