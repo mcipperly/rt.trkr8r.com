d51d5114d59b22869656a0b9d541cdfdfe011b76
rob <topia17@gmail.com>
2015-07-08 20:04:08 -0400

    Added ability to update the hours or company

--- admin/manage-hours.php
+++ admin/manage-hours.php
@@ -10,19 +10,30 @@ $service_date = date("Y-m-d", strtotime($readable_service_date));
 $companies = get_companies();
 
 if($_REQUEST['record']) {
-	foreach($_REQUEST as $name => $value) {
-		if(substr_count($name, "duration")) {
-			$name_array = explode("_", $name);
-			$volunteer_id = $name_array[1];
-			record_volunteer_time($volunteer_id, $value, $service_date);
-		}
-		
-		if(substr_count($name, "company_id")) {
-			$name_array = explode("_", $name);
-			$volunteer_id = $name_array[2];
-			record_volunteer_company($volunteer_id, $value);
-		}
+	if($_REQUEST['use_all']) {
+		if($_REQUEST['duration_all'])
+			record_all_volunteer_time($_REQUEST['duration_all'], $service_date);
+		if($_REQUEST['company_id_all'])
+			record_all_volunteer_company($_REQUEST['company_id_all'], $service_date);
 	}
+	else
+		foreach($_REQUEST as $name => $value) {
+			if(substr_count($name, "duration")) {
+				$name_array = explode("_", $name);
+				if($name_array[1] == "all")
+					continue;
+				$volunteer_id = $name_array[1];
+				record_volunteer_time($volunteer_id, $value, $service_date);
+			}
+		
+			if(substr_count($name, "company_id")) {
+				$name_array = explode("_", $name);
+				if($name_array[2] == "all")
+					continue;
+				$volunteer_id = $name_array[2];
+				record_volunteer_company($volunteer_id, $value);
+			}
+		}
 }
 
 $volunteers = get_volunteers_of_day($service_date);
@@ -80,6 +91,7 @@ foreach($volunteers as $key => $volunteer) {
 <form method="POST">
 	<input type="hidden" name="record" value="1" />
 	<input type="hidden" name="service_date" value="{$service_date}" />
+	<input type="hidden" name="use_all" id="use_all" value="" />
     <h4 class="left">Volunteer Name</h4>
     <h4 class="center">Affiliation</h4>
     <h4 class="right" style="padding-right:10px">Hours</h4>
@@ -128,6 +140,38 @@ EOS;
 	if($key + 1 == sizeof($volunteers)) {
 		$html = <<<EOS
     <input type="submit" value="Log Hours" class="right">
+    <br class="clear">
+    <div class="log_vol-name">
+        <span class="left">Update All Volunteers</span>
+EOS;
+		print($html);
+	
+		foreach($companies as $comp_key => $company) {
+			if($comp_key == 0) {
+				$html = <<<EOS
+		<select name="company_id_all" class="">
+EOS;
+				print($html);
+			}
+		
+			$html = <<<EOS
+			<option label="{$company['name']}" value="{$company['company_id']}">{$company['name']}</option>
+EOS;
+			print($html);
+		
+			if($comp_key + 1 == sizeof($companies)) {
+				$html = <<<EOS
+		</select>
+EOS;
+			print($html);
+			}
+		}
+	
+		$html = <<<EOS
+        <input type="text" class="right" name="duration_all" size="3" value="">
+    </div>
+    <br class="clear">
+    <input type="submit" value="Log Hours For All" onclick="document.getElementById('use_all').value=1;return true;" class="right">
     <div class="clear"></div>
 </form>
 </div>
