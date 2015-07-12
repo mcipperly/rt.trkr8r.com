44bf94abcc6036cee79d6f13add470c9d85bcc26
Amy DePalma <amy@amymade.net>
2015-07-12 16:01:01 -0400

    Admin Sidebar Update
    - Deleted "Manage Hours" as these will now go into the "Manage Events"
    page
    - Updated the wording of the form page to reflect users will only be
    managing one form, not multiple

--- includes/admin-sidebar.php
+++ includes/admin-sidebar.php
@@ -21,11 +21,10 @@
     <nav>
         <ul>
             <li><a href="/admin/"><span class="fa fa-home"></span>&nbsp;&nbsp;Dashboard</a></li>
-            <li><a href="/admin/manage-forms.php"><span class="fa fa-gear"></span>&nbsp;&nbsp;Manage Forms</a></li>
+            <li><a href="/admin/manage-form.php"><span class="fa fa-gear"></span>&nbsp;&nbsp;Manage Form</a></li>
             <li><a href="/admin/manage-events.php"><span class="fa fa-calendar"></span>&nbsp;&nbsp;Manage Events</a></li>
             <li><a href="/admin/export-records.php"><span class="fa fa-file-excel-o"></span>&nbsp;&nbsp;Export Records</a></li>
             <li><a href="/admin/print-records.php"><span class="fa fa-print"></span>&nbsp;&nbsp;Print Records</a></li>
-            <li><a href="/admin/manage-hours.php"><span class="fa fa-book"></span>&nbsp;&nbsp;Manage Hours</a></li>
             <li><a href="/admin/manage-users.php"><span class="fa fa-group"></span>&nbsp;&nbsp;Manage Users</a></li>
             <li><a href="#api"><span class="fa fa-paint-brush"></span>&nbsp;&nbsp;Customize Theme</a></li>
             <li><a href="/admin/onsite.php"><span class="fa fa-toggle-off"></span>&nbsp;&nbsp;Activate On-Site Mode</a></li>
