b7a66ebfc8079c69419af73b5a72cf07c11b72ff
Cipperly, Matt <CipperlyM@AE.com>
2015-07-20 22:22:58 -0400

    all signatures no longer go to statically defined directory

--- capture-signature.php
+++ capture-signature.php
@@ -5,7 +5,7 @@ $data_pieces = explode(",", $_REQUEST['signature-b64']);
 $encoded_image = $data_pieces[1];
 $decoded_image = base64_decode($encoded_image);
 $filename = "Signature-" . $_REQUEST['firstname'] . $_REQUEST['lastname'] . "-" . date("Ymd") . ".png";
-$result = file_put_contents("/usr/local/www/sub/rt.trkr8r.com/signatures/" . $filename, $decoded_image);
+$result = file_put_contents(getcwd() . "/signatures/" . $filename, $decoded_image);
 print_r($result);
 exit;
 
