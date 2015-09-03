a30315fe2d545c89acfbc63e1b6ac47845669f7e
rob <topia17@gmail.com>
2015-09-02 20:00:14 -0400

    Added functionality to completed events tile in both manage events and completed events

--- admin/completed-events.php
+++ admin/completed-events.php
@@ -2,6 +2,7 @@
 include ('../includes/admin-header.php'); 
 include('validate.php');
 include ('../includes/admin-sidebar.php'); 
+include '../db/db.php';
 ?>
 
 <div class="container">
@@ -11,7 +12,6 @@ include ('../includes/admin-sidebar.php');
 <div class="row">
     <div class="twelve cols callout">
         <h2 class="callout-title">Completed Events</h2> 
-            
             <table>
                 <thead>
                     <tr>
@@ -22,111 +22,84 @@ include ('../includes/admin-sidebar.php');
                         <th>Edit</th>
                     </tr>
                 </thead>
-                
                 <tbody>
+<?php
+$page = (int) $_REQUEST['page'];
+
+if($_REQUEST['toggle_event_id']) {
+	toggle_event_status($_REQUEST['toggle_event_id']);
+}
+
+//Find the twelve oldest (second twelve oldest etc) completed events
+$search['status_id'] = 2; //Closed
+$search['sort_dir'] = 1; //DESC
+$search['count'] = 12;
+$search['offset'] = 12 * $page;
+$events = get_events($search);
+
+//If there are no events for this non-first page, go back a page
+if(!$events && $page > 0) {
+	$prev_page = $page - 1;
+
+	Header("HTTP/1.1 302 Moved Temporarily");
+	Header("Location: /admin/completed-events.php?page={$prev_page}");
+}
+
+foreach($events as $event) {
+	$event_date = date("m/d/Y", strtotime($event['date']));
+	$event_vols = number_format($event['totals']['volunteers']);
+	$event_dur = number_format($event['totals']['duration'], 2);
+	$html = <<<EOS
                     <tr>
-                        <td data-label="Event">This is a Completed Event</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
+                        <td data-label="Event">{$event['location']}</td>
+                        <td data-label="Date">{$event_date}</td>
+                        <td data-label="Volunteers">{$event_vols}</td>
+                        <td data-label="Hours">{$event_dur}</td>
+                        <td data-label="Details"><a href="completed-events.php?toggle_event_id={$event['event_id']}&page={$page}"><button>View</button></a></td>
                     </tr>
-                    
-                    <tr>
-                        <td data-label="Event">Event Name</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
-                    </tr>
-                    
-                    <tr>
-                        <td data-label="Event">Event Name</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
-                    </tr>
-                    
-                    <tr>
-                        <td data-label="Event">Event Name</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
-                    </tr>
-                    
-                    <tr>
-                        <td data-label="Event">Event Name</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
-                    </tr>
-                    
-                    <tr>
-                        <td data-label="Event">Event Name</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
-                    </tr>
-                    
-                    <tr>
-                        <td data-label="Event">Event Name</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
-                    </tr>
-                    
-                    <tr>
-                        <td data-label="Event">Event Name</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
-                    </tr>
-                    
-                    <tr>
-                        <td data-label="Event">Event Name</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
-                    </tr>
-                    
-                    <tr>
-                        <td data-label="Event">Event Name</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
-                    </tr>
-                    
-                    <tr>
-                        <td data-label="Event">Event Name</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
-                    </tr>
-                    
-                    <tr>
-                        <td data-label="Event">Event Name</td>
-                        <td data-label="Date">00/00/00</td>
-                        <td data-label="Volunteers">1,000</td>
-                        <td data-label="Hours">100,000</td>
-                        <td data-label="Edit"><a href="#"><button>Reopen</button></a></td>
-                    </tr>
+EOS;
+	print($html);
+}
+?>
                 </tbody>
             </table>
+<?php
+$show_left_link = ($page > 0);
+$show_right_link = (sizeof($events) == 12);
+
+if($show_left_link || $show_right_link) {
+	$html = <<<EOS
             <div>
+EOS;
+	print($html);
+}
+
+if($show_left_link) {
+	$html = <<<EOS
+                <div class="left"><a href="#"><span class="fa fa-arrow-circle-left"></span> View Newer</a></div>
+EOS;
+	print($html);
+}
+if($show_right_link) {
+	$html = <<<EOS
                 <div class="right"><a href="#">View Older <span class="fa fa-arrow-circle-right"></span></a></div>
+EOS;
+	print($html);
+}
+
+if($show_left_link || $show_right_link) {
+	$html = <<<EOS
+                <div class="clear"></div>
+            </div>
+EOS;
+	print($html);
+}
+?>
+    </div>   
+            <div>
                 <div class="left"><a href="manage-events.php"><span class="fa fa-calendar"></span> Back to Manage Events</a></div>
                 <div class="clear"></div>
             </div>
-    </div>   
 </div>  
 
 <?php include ('../includes/footer.php'); ?>
