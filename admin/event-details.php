d47e89d7af59e6ae6f2d0ad1570acadefb71e8b0
Cipperly, Matt <CipperlyM@AE.com>
2015-09-30 21:34:41 -0400

    include event_id to get event-specific signature

--- admin/event-details.php
+++ admin/event-details.php
@@ -282,7 +282,7 @@ EOS;
 
 		$html .= <<<EOS
 						<tr>
-							<td data-label="Print Details" class="print_details"><a href="javascript: w=window.open('../signature.php?view=1&vid={$volunteer['volunteer_id']}'); w.print()"><span class="fa fa-print fa-lg"></span></a></td>
+							<td data-label="Print Details" class="print_details"><a href="javascript: w=window.open('../signature.php?view=1&vid={$volunteer['volunteer_id']}&event_id={$event['event_id']}'); w.print()"><span class="fa fa-print fa-lg"></span></a></td>
 							<td data-label="Volunteer Name">{$volunteer['firstname']} {$volunteer['lastname']}</td>
 							<td data-label="Affiliation">{$volunteer['company']['name']}</td>
 							<td data-label="Hours">{$volunteer['duration']}</td>
