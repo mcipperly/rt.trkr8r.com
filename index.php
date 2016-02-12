1baf0afcbfc9347f7aaad5e01f949d37dc6b6fb0
Cipperly, Matt <CipperlyM@AE.com>
2016-02-12 00:30:33 -0500

    remove modal for email input on main page

--- index.php
+++ index.php
@@ -8,49 +8,17 @@ $events = get_events($search);
 ?>
 
 <script type="text/javascript">
-function validateEmail(email) { 
+function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
-} 
-function getEmail() {
-vex.dialog.prompt({
-  message: 'Please enter your email address:',
-  // placeholder: 'Email Address',
-  input: '<input name="email" type="email" class="vex-dialog-prompt-input" placeholder="email@example.com" value=""><input type="checkbox" name="under18" id="under18" value="true"><label style="display:inline" for="under18"> <b>Under 18?</b> Check here to print our waiver!</label>',
-  callback: function(value) {
-    if(value.email||value.under18) {
-      if(value.under18 === "true") {
-        window.location.assign("/under-18.php");
-      } else {
-        if(validateEmail(value.email)) {
-          window.location.assign("/register.php?email="+encodeURIComponent(value.email));
-        } else {
-          invalidEmail(value.email);
-        }
-      }
-    }
-  }
-});
 }
-function invalidEmail(email) {
-vex.dialog.prompt({
-  message: '<div style="color:#FF0000">Please enter a valid email address:</div>',
-  placeholder: 'Email Address',
-  input: '<input name="email" type="email" class="vex-dialog-prompt-input" placeholder="email@example.com" value=""><input type="checkbox" name="under18" id="under18" value="true"><label style="display:inline" for="under18"> <b>Under 18?</b> Check here to print our waiver!</label>',
-  callback: function(value) {
-    if(value.email||value.under18) {
-      if(value.under18 === "true") {
-        window.location.assign("/under-18.php");
-      } else {
-        if (validateEmail(value.email)) {
-          window.location.assign("/register.php?email="+encodeURIComponent(value.email));
-        } else { 
-          invalidEmail(value.email);
-        }
-      }
-    }
+function getEmail(email) {
+  if(!validateEmail(email)) {
+    document.getElementById('email').placeholder='Invalid email address entered';
+    document.getElementById('email').value='';
+    document.getElementById('email').style='border:1px solid red';
+    return false
   }
-});
 }
 </script>
 
@@ -60,7 +28,7 @@ vex.dialog.prompt({
 var vexdiag = vex.dialog.alert('<?php if($_GET['thanks'] == 1) { ?>Thanks for registering for <?php print($site_org) ?>!');
 setTimeout(function() {
    vex.close(vexdiag.data().vex.id); }, 3000);<?php } elseif($_GET['thanks'] == 2) { ?>Thanks for registering with <?php print($site_org) ?>! Please visit the registration table upon arrival at your event!');<?php } else { ?>We\'ve already pre-registered you for <?php print($site_org) ?> - please check in with an event coordinator upon arrival to your event. Thank you!');<?php } ?>
-</script>  
+</script>
 <?php } ?>
 
 <div class="center">
@@ -69,12 +37,10 @@ setTimeout(function() {
 
 <div class="row center welcome">
     <div class="twelve cols">
-        <input name="email" type="email" placeholder="email@example.com" value="" class="register-email">
-      
-        <a href="#">
-            <button onclick="getEmail()"><?php if(isset($_SESSION['mode'])) { print('Check In'); } else { print('Pre-register'); } ?></button>
-        </a>
-        
+      <form onsubmit="return getEmail(document.getElementById('email').value)" method="GET" action="/register.php">
+        <input name="email" type="email" placeholder="email@example.com" id='email' value="" class="register-email">
+        <button type="submit"><?php if(isset($_SESSION['mode'])) { print('Check In'); } else { print('Pre-register'); } ?></button>
+      </form>
         <br>
         <a href="/under-18.php" class="under-18">
             <b>Under 18?</b> Click here to print our waiver!
