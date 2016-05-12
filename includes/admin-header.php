a1bfa52afff9fda586a1864d31bf7f0bc562a28a
Amy DePalma <amy@2206design.com>
2016-05-12 14:12:40 -0400

    Font Awesome Embed Code added
    No more self-hosting; better for keeping these up-to-date

--- includes/admin-header.php
+++ includes/admin-header.php
@@ -28,6 +28,7 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>  
     <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
     <script src="http://code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+    <script src="https://use.fontawesome.com/e1c94424d2.js"></script>
     <script src="/assets/js/admin-sidebar.js"></script>
 </head>
 
