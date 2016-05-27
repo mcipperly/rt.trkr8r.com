82f3f6f52a8b5473943b0d93fdc9141c62cfa14b
Matthew Cipperly <mc@tinyatom.net>
2016-05-26 20:36:38 -0400

    #110 feedback on delete org function

--- admin/manage-orgs.php
+++ admin/manage-orgs.php
@@ -8,6 +8,7 @@ foreach($_REQUEST as $key => $value) {
 	if(substr_count($key, "remove_")) {
 		$val_arr = explode('_', $key);
 		invalidate_organization($val_arr[1]);
+		$org_deleted = 1;
 	}
 }
 
@@ -28,6 +29,11 @@ EOS;
 elseif($_REQUEST['org_name'] && !$success) {
     $success_html = <<<EOS
 		<div class="row"><div class="twelve cols callout failure">Organization not created. Please ensure all fields are properly filled in.</div></div>
+EOS;
+}
+elseif(isset($org_deleted) || $_GET['deleted'] == "success") {
+	$success_html = <<<EOS
+	<div class="row"><div class="twelve cols callout success">Organization deleted!</div></div>
 EOS;
 }
 
